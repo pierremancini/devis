00ef294c7e95d4a6f635f6feaa529c7f17ce1258
Mancini <pierremncini@gmail.com>
2020-04-26 10:39:33 +0200

    Passage de variable au template via jinja

--- printer.py
+++ printer.py
@@ -1,8 +1,32 @@
 from weasyprint import HTML, CSS
+import argparse
+from jinja2 import Template
+from datetime import date
+import io
 
 # Commande: python3 printer.py
 
-html = HTML(filename='devis_site_petit.html')
-css = CSS(filename='devis.css')
+parser = argparse.ArgumentParser(description="receuille les noms de fichiers pour construir le devis.")
+parser.add_argument('--html', help='Nom et chemin de fichier html')
+parser.add_argument('--css', help='Nom et chemin de fichier css')
+parser.add_argument('--out', help='Nom et chemin du fichier de sortie')
+args = parser.parse_args()
 
-html.write_pdf('devis_site_petit.pdf')
+# Partie jinja
+def num_devis():
+    # on utilise os.walk, len
+    return 12
+
+# Passe le template sous forme d'objet fichier en memoire 
+# et pas sous forme de nom de fichier à lire
+with open(args.html, 'r') as file_object:
+
+    # Modif jinja sur le file_object
+    t = Template(file_object.read())
+    fdate = date.today().strftime('%d/%m/%Y')
+    # html = HTML(io.StringIO(t.render(num_devis=num_devis(), date=fdate)))
+    html = HTML(string=t.render(num_devis=num_devis(), date=fdate))
+
+css = CSS(filename=args.css)
+
+html.write_pdf(args.out)
