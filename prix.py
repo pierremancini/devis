220c2102e4b14988865d45f718883343de3c8474
Mancini <pierremncini@gmail.com>
2020-04-29 15:05:59 +0200

    Calcul des montants/sous-totaux

--- prix.py
+++ prix.py
@@ -2,13 +2,17 @@ import decimal
 
 hebergement = {
     'grille': [
-            {'designation': 'Location du serveur chez gandi.net' , 'quantité': 1, 'prix': 1.1 },
+            {'designation': 'Location du serveur chez gandi.net' , 'quantité': 4, 'prix_unit': 1.1 },
             {'designation': 'Location du nom de domaine "labrouche.notaire.fr"',
-                'quantité': 1,
-                'prix': 2.2 },
+                'quantité': 2,
+                'prix_unit': 2.2 },
             {'designation': "Gestion du site, incluant: <br/> accès aux consoles, suivit du trafic, interventions d'urgences" ,
                 'quantité': 1,
-                'prix': 1.1 },
+                'prix_unit': 1.1 },
         ],
     'devise': '€ / mois',
 }
+
+# calcul des montant/sous-totaux
+for line in hebergement['grille']:
+    line['montant'] = line['quantité'] * line['prix_unit']
