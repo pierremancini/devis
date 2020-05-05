060d71f9d62e38847330dd13579667e4f01d8dea
Mancini <pierremncini@gmail.com>
2020-05-05 13:16:44 +0200

    Utilisation de puppeteer pour imprimer pdf avece header et footer correct.

---
+++ print-puppeteer.js
@@ -0,0 +1,29 @@
+// node print-puppeteer.js 
+// node -v -> v14.1.0
+
+
+const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
+ 
+(async function printPDF() {
+  try {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], executablePath: "/snap/bin/chromium", headless: true});
+    const page = await browser.newPage();
+    const html = await fs.readFile('tmp_out_jinja.html', 'utf-8')
+    console.log(html)
+    await page.setContent(html)
+    await page.addStyleTag({path:'templates/devis.css'})
+    const pdf = await page.pdf({ 
+      path: 'out_puppeteer.pdf',
+      format: 'A4',
+      displayHeaderFooter: true,
+      headerTemplate: '<div class="text" id="pageHeader">Devis n° 12 - 30/04/2020</div>',
+      footerTemplate: '<div class="text center"><span class="pageNumber"></span></div>'
+    });
+   
+    console.log('done')
+    await browser.close();
+  } catch (e) {
+    console.log('Erreur:',e);
+  }
+})();
--- render.py
+++ render.py
@@ -1,4 +1,3 @@
-from weasyprint import HTML, CSS
 import argparse
 from jinja2 import Template
 from datetime import date
@@ -10,8 +9,7 @@ import client
 # Commande: python3 printer.py
 
 parser = argparse.ArgumentParser(description="receuille les noms de fichiers pour construir le devis.")
-parser.add_argument('--html', help='Nom et chemin de fichier html')
-parser.add_argument('--css', help='Nom et chemin de fichier css')
+parser.add_argument('--jinja', help='Nom et chemin de fichier html')
 parser.add_argument('--out', help='Nom et chemin du fichier de sortie')
 args = parser.parse_args()
 
@@ -42,18 +40,13 @@ template_var = {
     'data_price': prix.hebergement
 }
 
-
-
 # Passe le template sous forme d'objet fichier en memoire 
 # et pas sous forme de nom de fichier à lire
-with open(args.html, 'r') as file_object:
+with open(args.jinja, 'r') as jinja_file, open(args.out, 'w') as html_file:
 
     # Modif jinja sur le file_object
-    t = Template(file_object.read())
+    t = Template(jinja_file.read())
     # html = HTML(io.StringIO(t.render(num_devis=num_devis(), date=fdate)))
-    out_jinja = t.render(**template_var)
-    html = HTML(string=out_jinja)
+    string_html = t.render(**template_var)
 
-css = CSS(filename=args.css)
-
-html.write_pdf(args.out, stylesheets=[css])
+    html_file.write(string_html)
