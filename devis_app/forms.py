2ed9ee6a3ee6a62ea8959385c8e92281e849deed
Mancini <pierremncini@gmail.com>
2020-07-06 11:07:37 +0200

    affiche tout le temps deux chiffres après la virgule

--- devis_app/forms.py
+++ devis_app/forms.py
@@ -5,17 +5,47 @@ from .models import Devis, Emeteur, Client, GrillePrix
 class DevisForm(forms.ModelForm):
     class Meta:
         model = Devis
-        fields = ['titre', 'date_creation', 'date_emission', 'num_emission']
+        fields = ['titre', 'date_creation', 'date_emission', 'num_emission', 'mention_total', 'mention']
+
+FIELD_NAME_EMETTEUR = {
+    'nom': 'nom_emetteur',
+    'adresse': 'adresse_emetteur',
+    'email':'email_emetteur',
+    'telephone':'telephone_emetteur',
+    'fax':'fax_emetteur'
+}
 
 class EmetteurForm(forms.ModelForm):
     class Meta:
         model = Emeteur
         fields = '__all__'
 
+    # https://stackoverflow.com/questions/8801910/override-django-form-fields-name-attr
+    def add_prefix(self, field_name):
+        # look up field name; return original if not found
+        field_name = FIELD_NAME_EMETTEUR.get(field_name, field_name)
+        return super(EmetteurForm, self).add_prefix(field_name)
+
+
+FIELD_NAME_CLIENT = {
+    'nom': 'nom_client',
+    'adresse': 'adresse_client',
+    'email': 'email_client',
+    'telephone': 'telephone_client',
+    'fax': 'fax_client'
+}
+
+
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = '__all__'
+
+    def add_prefix(self, field_name):
+        # look up field name; return original if not found
+        field_name = FIELD_NAME_CLIENT.get(field_name, field_name)
+        return super(ClientForm, self).add_prefix(field_name)
+
 
 class GrillePrixForm(forms.ModelForm):
     class Meta:
