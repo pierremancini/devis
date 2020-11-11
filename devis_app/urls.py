eb33fad6a2aeb0c09ba220ae093d095644941609
Mancini <pierremncini@gmail.com>
2020-11-11 19:24:42 +0100

    Module non utilisé.

--- devis_app/urls.py
+++ devis_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from puppeteer_pdf.views import PDFTemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
