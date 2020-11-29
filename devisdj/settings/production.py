126e3910e78c4a4aec1e907c60df84ee6b71b723
Mancini <pierremncini@gmail.com>
2020-11-29 18:13:33 +0100

    Log des erreurs en prod.

--- devisdj/settings/production.py
+++ devisdj/settings/production.py
@@ -13,6 +13,43 @@ ALLOWED_HOSTS = ['127.0.0.1', 'pierremancini.fr', '155.133.129.205']
 STATIC_URL = '/gestion/static/'
 STATIC_ROOT = '/var/www/devis/static/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'basic': {
+            'format': '%(asctime)s %(name)-20s %(levelname)-8s %(message)s',
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+            'formatter': 'basic',
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'INFO'
+    },
+}
+
 try:
     from .local import *
 except ImportError:
